7393a8a98fa69d3fb20de60c72c4a56a24551d86
fernando callata <fernando.henrique@geopix.com.br>
2016-12-01 04:12:21 -0200

    não consegui cadastrar um evento, pois as regionais estoa como  list, nao entendi porque colocou a regional como list sendo que vai cadastrar o id

--- src/main/java/br/ufg/inf/quintacalendario/view/console/CadastroEvento.java
+++ src/main/java/br/ufg/inf/quintacalendario/view/console/CadastroEvento.java
@@ -14,50 +14,30 @@ import br.ufg.inf.quintacalendario.view.console.util.EntradaConsole;
  * @author fer
  */
 public class CadastroEvento extends AbstractTelaCabecalho {
-    private String dataInicial;
-    private String dataFinal;
-    private int instituto;
-    private int regional;
-    private String nomeEvento;
+ 
+    public CadastroEvento(PrintStream output) {
+            super(output);
+    }
 
-	public CadastroEvento(PrintStream output) {
-		super(output);
-	}
-
-	public void index() {
+    public void index() {
         this.exibaCabecalho();
         this.telaCadastro();
     }
     
     private void telaCadastro() {
         EntradaConsole evento = new EntradaConsole();
-        this.nomeEvento = evento.pergunteString("Insira o nome do Evento", true);
-        this.instituto  = evento.pergunteInteiro("Insira o instituto que o "
+        //O método pergunteString, faz a pergunta e retorna a string escrita
+        //pelo usuário
+        evento.pergunteString("Insira o nome do Evento", true);
+        //Retorna a resposta inteiro do usuário
+        evento.pergunteInteiro("Insira o instituto que o "
         + "evento pertence");
-        this.dataInicial = evento.pergunteString("Insira a data de ínicio do"
+        evento.pergunteString("Insira a data de ínicio do"
                 + " evento", true);
-        this.dataFinal = evento.pergunteString("Insira a data do término do "
+        evento.pergunteString("Insira a data do término do "
                 + "evento", true);
-        this.regional = evento.pergunteInteiro("Insira a regional do evento");
+        evento.pergunteInteiro("Insira a regional do evento");
     }
     
-    private String getDataInicial() {
-        return dataInicial;
-    }
     
-    private String getDataFinal() {
-        return dataFinal;
-    }
-    
-    private int getRegional() {
-        return regional;
-    }
-    
-    private String getNomeEvento() {
-        return nomeEvento;
-    }
-    
-    private int getInstituto() {
-        return instituto;
-    }
 }
