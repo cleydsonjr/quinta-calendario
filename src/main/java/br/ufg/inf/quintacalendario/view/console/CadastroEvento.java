9433f7ea15ed2a8af7347967dcf7880fa99c1520
fernando callata <fernando.henrique@geopix.com.br>
2016-12-01 01:48:59 -0200

    tela de cadastro, capturando informações para salvar no banco

--- src/main/java/br/ufg/inf/quintacalendario/view/console/CadastroEvento.java
+++ src/main/java/br/ufg/inf/quintacalendario/view/console/CadastroEvento.java
@@ -5,17 +5,32 @@
  */
 package br.ufg.inf.quintacalendario.view.console;
 
+import br.ufg.inf.quintacalendario.view.console.util.EntradaConsole;
+
 /**
  *
  * @author fer
  */
 public class CadastroEvento extends AbstractTelaCabecalho {
-    
+    private String dataInicial;
+    private String dataFinal;
+    private String instituto;
+    private String regional;
+    private String nomeEvento;
     public void index() {
         this.exibaCabecalho();
     }
     
     private void telaCadastro() {
-        
+        EntradaConsole evento = new EntradaConsole();
+        this.nomeEvento = evento.pergunteString("Insira o nome do Evento", true);
+        this.instituto  = evento.pergunteString("Insira o instituto que o "
+        + "evento pertence", true);
+        this.dataInicial = evento.pergunteString("Insira a data de ínicio do"
+                + " evento", true);
+        this.dataFinal = evento.pergunteString("Insira a data do término do "
+                + "evento", true);
+        this.regional = evento.pergunteString("Insira a regional do evento"
+        + "", true);
     }
 }
