a6ccc343e4086e18e7a7eccc5e41f87d72dfc5c4
trete <flytrete@hotmail.com>
2016-12-08 02:23:11 -0200

    Add files via upload

--- src/main/java/br/ufg/inf/quintacalendario/model/Categoria.java
+++ src/main/java/br/ufg/inf/quintacalendario/model/Categoria.java
@@ -5,24 +5,36 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Classe que modela a representação das categorias de evento no banco de dados.
+ */
 @Entity(name = "categoria")
 public class Categoria {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	private String nome;
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    /**
+      * Código de identificação da categoria de evento.
+     */
+    private long id;
+    /**
+      * Nome da categoria do evento.
+     */
+    private String nome;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
